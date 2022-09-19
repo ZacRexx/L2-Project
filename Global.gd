dcd32f2592b8d5d774a06ba3c2c1237060883400
ZacRexx <liam.adams@me.com>
2022-09-19 12:56:45 +1200

    Commit

--- Global.gd
+++ Global.gd
@@ -4,7 +4,7 @@ extends Node2D
 # When the action reset button is pressed the folling 
 #fuction will happen resent current scene and set the 
 #players spawn back to null
-
+#git error 
 func _process(_delta):
 	if Input.is_action_just_pressed("Reset"):
 		get_tree().reload_current_scene()
